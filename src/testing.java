0e7e50052faddc4968e8f90d8c4ebbdd99b02291
Kyle Schang <kyle@schang.net>
2023-02-07 23:41:34 -0500

    cleaning up programs

--- src/testing.java
+++ src/testing.java
@@ -5,51 +5,6 @@ public class testing {
 
     public static void main(String[] args) {
 
-        String s = "test ";
-
-        int num1 = 1;
-        int num2 = 2;
-        int num3 = 3;
-
-        s = new StringBuilder().append(s).append("(").append(num1).append(",").append(num2).append(")").toString();
-
-        System.out.println(s);
-
-        // This is me adding a test to the testing file
 
     }
-
-    public static double mean(double[] x){
-
-        double total = 0;
-        for (int i = 0; i < 10; ++ i){
-            total = total + x[i];
-        }
-
-        return total/10;
-
-    }
-
-    public static double deviation (double[] x){
-
-        double total = 0;
-        for (int i = 0; i < 10; ++ i){
-            total = total + x[i];
-        }
-
-        double mean = total / 10;
-
-        double total_minus_mean = 0;
-
-        for (int i = 0; i < 10; ++i){
-            double value = x[i] - mean;
-            value = value * value;
-            total_minus_mean = total_minus_mean + value;
-        }
-
-        double under_radical = total_minus_mean / 9;
-        return Math.sqrt(under_radical);
-
-    }
-
 }
