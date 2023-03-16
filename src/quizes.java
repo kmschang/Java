319c6ace379e99122c813dabc0549915d3646c75
Kyle Schang <kyle@schang.net>
2023-03-16 19:07:15 -0400

    quiz 7

---
+++ src/quizes.java
@@ -0,0 +1,11 @@
+public class quizes {
+
+    public static void main(String[] args) {
+
+        System.out.print("A,B;C".replaceAll(",;", "#") + " ");
+        System.out.println("A,B;C".replaceAll("[,;]", "#"));
+
+
+    }
+
+}
