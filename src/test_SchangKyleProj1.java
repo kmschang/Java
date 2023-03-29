6c581afbe887fe5e82f6e53037d1ea1b4a56d19f
Kyle <Kyle@schang.net>
2023-03-29 13:30:13 -0400

    adding test cases

--- src/test_SchangKyleProj1.java
+++ src/test_SchangKyleProj1.java
@@ -14,13 +14,14 @@ public class test_SchangKyleProj1 {
     private Assertions Assert;
     int[][] test_answers = new int[3][3];
 
+    // {{},{},{},{},{},{},{},{},{}}
 
     @Test
-    @DisplayName("One Missing")
-    void one_missing(){
-        SchangKyleProj1.board = new int[][] {{7,2,4,8,6,5,1,3,9},{5,1,9,2,4,3,8,7,6},{3,0,6,7,9,1,5,4,2},{1,7,8,6,2,9,4,5,3},{9,4,3,1,5,8,2,6,7},{6,5,2,3,7,4,9,1,8},{2,3,1,5,8,6,7,9,4},{8,9,5,4,3,7,6,2,1},{4,6,7,9,1,2,3,8,5}};
-        test_answers = new int[][] {{2,1,8},{0,0,0},{0,0,0}};
-        SchangKyleProj1.location = new int[][] {{2,1},{0,0},{0,0}};
+    @DisplayName("(3,0,8)")
+    void three_zero_eight(){
+        SchangKyleProj1.board = new int[][] {{1,2,3,4,5,6,7,8,9},{4,5,6,7,8,9,1,2,3,},{7,8,9,1,2,3,4,5,6},{0,1,2,3,4,5,6,9,7},{3,4,5,6,9,7,2,1,8,},{6,9,7,2,1,8,3,4,5,},{2,3,8,5,6,1,9,7,4,},{5,6,1,9,7,4,8,3,2,},{9,7,4,8,3,2,5,6,1,}};
+        test_answers = new int[][] {{3,0,8},{0,0,0},{0,0,0}};
+        SchangKyleProj1.location = new int[][] {{3,0},{0,0},{0,0}};
         SchangKyleProj1.zeros = 1;
         SchangKyleProj1.solveBoard();
         for (int i = 0; i < 3; ++ i){
@@ -29,6 +30,27 @@ public class test_SchangKyleProj1 {
             }
         }
     }
+
+    @Test
+    @DisplayName("(8,8,9)")
+    void eight_eight_nine(){
+        SchangKyleProj1.board = new int[][] {{1,2,3,4,5,6,7,9,8},{4,5,6,7,8,9,1,2,3},{7,8,9,1,2,3,4,5,6},{2,1,4,3,6,5,9,8,7},{3,6,5,8,9,7,2,1,4},{8,9,7,2,1,4,3,6,5},{5,3,1,9,4,8,6,7,2},{9,4,8,6,7,2,5,3,1},{6,7,2,5,3,1,8,4,0}};
+        test_answers = new int[][] {{8,8,9},{0,0,0},{0,0,0}};
+        SchangKyleProj1.location = new int[][] {{8,8},{0,0},{0,0}};
+        SchangKyleProj1.zeros = 1;
+        SchangKyleProj1.solveBoard();
+        for (int i = 0; i < 3; ++ i){
+            for (int j = 0; j < 3; ++ j){
+                assertEquals(test_answers[i][j], SchangKyleProj1.answers[i][j]);
+            }
+        }
+    }
+
+
+
+
+
+
 
 
 }
