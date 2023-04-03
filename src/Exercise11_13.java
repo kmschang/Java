26ef2bd5aa8e3f96edc6b015df067b815267f781
Meaneac <kyle@schang.net>
2023-04-03 16:40:22 -0400

    clang

--- src/Exercise11_13.java
+++ src/Exercise11_13.java
@@ -3,41 +3,33 @@ import java.util.Scanner;
 
 public class Exercise11_13 {
 
+  public static void removeDuplicate(ArrayList<Integer> list) {
+    ArrayList<Integer> temp = new ArrayList<Integer>();
 
-    public static void removeDuplicate(ArrayList<Integer> list) {
-        ArrayList<Integer> temp = new ArrayList<Integer>();
-
-        for (int i = 0; i < list.size(); ++ i){
-            if (!temp.contains(list.get(i))){
-                temp.add(list.get(i));
-            }
-        }
-
-        list.clear();
-        list.addAll(temp);
+    for (int i = 0; i < list.size(); ++i) {
+      if (!temp.contains(list.get(i))) {
+        temp.add(list.get(i));
+      }
     }
 
-    public static void main(String[] args){
+    list.clear();
+    list.addAll(temp);
+  }
 
-        Scanner scnr = new Scanner(System.in);
-        System.out.println("Enter ten integers: ");
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = 0; i < 10; ++ i){
-            list.add(scnr.nextInt());
-        }
+  public static void main(String[] args) {
 
-        removeDuplicate(list);
-
-        System.out.println("The distinct integers are: ");
-        for (int i = 0; i < list.size(); ++ i){
-            System.out.print(list.get(i) + " ");
-        }
-
-
+    Scanner scnr = new Scanner(System.in);
+    System.out.println("Enter ten integers: ");
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    for (int i = 0; i < 10; ++i) {
+      list.add(scnr.nextInt());
     }
 
+    removeDuplicate(list);
 
+    System.out.println("The distinct integers are: ");
+    for (int i = 0; i < list.size(); ++i) {
+      System.out.print(list.get(i) + " ");
+    }
+  }
 }
-
-
-
