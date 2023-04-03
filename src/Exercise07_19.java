03165c97855da3553c9c98e51241e611f8b10abf
Meaneac <kyle@schang.net>
2023-04-03 15:54:43 -0400

    clang

--- src/Exercise07_19.java
+++ src/Exercise07_19.java
@@ -4,49 +4,46 @@
  * 02-02-23
  */
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Exercise07_19
-{
+public class Exercise07_19 {
 
-    public static void main(String[] args)
-    {
+  public static void main(String[] args) {
 
-        Scanner scnr = new Scanner(System.in);
-        System.out.println("Enter List: ");
+    Scanner scnr = new Scanner(System.in);
+    System.out.println("Enter List: ");
 
-        int num = scnr.nextInt();
+    int num = scnr.nextInt();
 
-        int[] numbers = new int[num];
-        // number of different integers
+    int[] numbers = new int[num];
+    // number of different integers
 
-        for (int k = 0; k < num; ++k){
-            numbers[k] = scnr.nextInt();
-        }
-
-        if (!isSorted(numbers)){
-            System.out.println("The list is not sorted");
-        } else {
-            System.out.println("The list is already sorted");
-        }
-
-
+    for (int k = 0; k < num; ++k) {
+      numbers[k] = scnr.nextInt();
     }
 
-    public static boolean isSorted(int[] list) {
-
-
-        int[] sorted_numbers = new int[list.length];
-        System.arraycopy(list, 0, sorted_numbers, 0, list.length);
-        Arrays.sort(sorted_numbers);
-
-        for (int j = 0; j < sorted_numbers.length; ++j){
-            if (list[j] != sorted_numbers[j]){
-                return false;
-            }
-        }
-        return true;
+    if (!isSorted(numbers)) {
+      System.out.println("The list is not sorted");
+    } else {
+      System.out.println("The list is already sorted");
     }
+  }
 
+  public static boolean isSorted(int[] list) {
+
+    int[] sorted_numbers = new int[list.length];
+    System.arraycopy(list, 0, sorted_numbers, 0, list.length);
+    Arrays.sort(sorted_numbers);
+
+    for (int j = 0; j < sorted_numbers.length; ++j) {
+      if (list[j] != sorted_numbers[j]) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
+
+// format this file
