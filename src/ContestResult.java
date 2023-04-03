14b14f46ef0ec8613e87cab6cd5c22d14c72c286
Meaneac <kyle@schang.net>
2023-04-03 17:30:37 -0400

    formatting

--- src/ContestResult.java
+++ src/ContestResult.java
@@ -1,31 +1,16 @@
 public class ContestResult {
 
-    private String winner = "";
-    private String secondPlace = "";
-    private String thirdPlace = "";
+  private String winner = "";
+  private String secondPlace = "";
+  private String thirdPlace = "";
 
+  public void setWinner(String winner) { this.winner = winner; }
+  public void setSecondPlace(String secondPlace) {
+    this.secondPlace = secondPlace;
+  }
+  public void setThirdPlace(String thirdPlace) { this.thirdPlace = thirdPlace; }
 
-
-    public void setWinner(String winner){
-        this.winner = winner;
-    }
-    public void setSecondPlace(String secondPlace){
-        this.secondPlace = secondPlace;
-    }
-    public void setThirdPlace(String thirdPlace){
-        this.thirdPlace = thirdPlace;
-    }
-
-
-    public String getWinner(){
-        return winner;
-    }
-    public  String getSecondPlace(){
-        return secondPlace;
-    }
-    public String getThirdPlace(){
-        return thirdPlace;
-    }
-
-
+  public String getWinner() { return winner; }
+  public String getSecondPlace() { return secondPlace; }
+  public String getThirdPlace() { return thirdPlace; }
 }
