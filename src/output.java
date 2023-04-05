c8b072de7777308c7f46d83bc1d082ec5e79e376
Meaneac <kyle@schang.net>
2023-04-04 21:31:14 -0400

    ran clang-format on output.java

--- src/output.java
+++ src/output.java
@@ -1,9 +1,7 @@
 class Main {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		System.out.println("Testing outputs");
-
-		}
-
-	}
+    System.out.println("Testing outputs");
+  }
+}
