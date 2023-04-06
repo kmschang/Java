e7ab256877fe8fb74eb5eb0907920634e43a3e8e
Meaneac <kyle@schang.net>
2023-04-06 16:42:35 -0400

    quiz 10

---
+++ src/Quiz.java
@@ -0,0 +1,12 @@
+public class Quiz {
+  public static void main(String[] args) {
+    try {
+      System.out.println("Welcome to Java");
+      int i = 0;
+      int y = 2 / i;
+      System.out.println("Welcome to HTML");
+    } finally {
+      System.out.println("The finally clause is executed");
+    }
+  }
+}
