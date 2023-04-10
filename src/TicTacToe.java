fea54750ecc14fdb48756f9525f5a910408cf82d
Meaneac <kyle@schang.net>
2023-04-10 18:45:44 -0400

    simplifying code on tic tac toe

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -15,9 +15,7 @@ public class TicTacToe {
 
   public static void main(String[] args) {
     loading();
-    starting_board();
     set_chars();
-    display_board();
     move();
   }
 
@@ -125,6 +123,9 @@ public class TicTacToe {
   }
 
   public static void set_chars() {
+
+    starting_board();
+
     char player = toUpperCase(pick_char());
     player_char = player;
 
@@ -137,6 +138,8 @@ public class TicTacToe {
     System.out.println("You are now \u001B[33m" + player_char + "\u001B[37m");
 
     board = new char[][] {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+
+    display_board();
   }
 
   public static void display_board() {
@@ -230,7 +233,7 @@ public class TicTacToe {
       col = 2;
     }
 
-    if (check_row_col(row, col)) {
+    if (check_location(row, col)) {
       board[row][col] = computer_char;
       ++move;
     } else {
@@ -238,48 +241,7 @@ public class TicTacToe {
     }
   }
 
-  public static boolean check_location(int location) {
-
-    int row;
-    int col;
-
-    if (location == 1) {
-      row = 0;
-      col = 0;
-    } else if (location == 2) {
-      row = 0;
-      col = 1;
-    } else if (location == 3) {
-      row = 0;
-      col = 2;
-    } else if (location == 4) {
-      row = 1;
-      col = 0;
-    } else if (location == 5) {
-      row = 1;
-      col = 1;
-    } else if (location == 6) {
-      row = 1;
-      col = 2;
-    } else if (location == 7) {
-      row = 2;
-      col = 0;
-    } else if (location == 8) {
-      row = 2;
-      col = 1;
-    } else {
-      row = 2;
-      col = 2;
-    }
-
-    if (board[row][col] == 'X' || board[row][col] == 'O') {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  public static boolean check_row_col(int row, int col) {
+  public static boolean check_location(int row, int col) {
 
     return board[row][col] != 'X' && board[row][col] != 'O';
   }
@@ -322,7 +284,7 @@ public class TicTacToe {
       col = 2;
     }
 
-    if (check_location(location)) {
+    if (check_location(row, col)) {
       board[row][col] = player_char;
       ++move;
     } else {
