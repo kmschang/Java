9d0456bed8f4674b511985fb391e1ab3a4055c5c
Meaneac <kyle@schang.net>
2023-04-11 12:37:06 -0400

    working on TTT

--- src/FirstIndex.java
+++ src/FirstIndex.java
@@ -1,10 +1,11 @@
 public class FirstIndex {
   public static void main(String[] args) {
-    int dim = 3;
+    int dim = 4;
+    int row = 0;
+    int col = 0;
     for (int num = 0; num < Math.pow(dim, 2); ++num) {
-      System.out.printin(num + 1);
-      System.out - printin("row=" + num / dim);
-      System.out.println("col=" + num % dim);
+      row = num / dim;
+      col = num % dim;
     }
-    System.out.printin("Or maybe I got the row/col backwards.");
   }
+}
