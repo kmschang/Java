ca45a0f8c44e9e6a6c0ab4fa7067674f1cbac7e1
Meaneac <kyle@schang.net>
2023-04-11 22:17:36 -0400

    removed unneeded code

--- src/FirstIndex.java
+++ src/FirstIndex.java
@@ -9,36 +9,3 @@ public class FirstIndex {
     }
   }
 }
-
-class testing_print {
-  public static void main(String[] args) {
-
-    thanks_for_playing();
-    title();
-  }
-
-  public static void thanks_for_playing() {
-    System.out.println();
-    System.out.println("\u001B[35m");
-    System.out.println("_____        _            ___   ___  __   _     _        _                ____");
-    System.out.println("  |   |__|  /_\\  |\\ | |/  |__   |__ |  | |_|   |_| |    /_\\   \\_/  | |\\ | | __");
-    System.out.println("  |   |  | /   \\ | \\| |\\  __|   |   |__| | \\   |   |__ /   \\   |   | | \\| |__|");
-  }
-
-  public static void title() {
-    System.out.print("\u001B[32m");
-    System.out.println("           _____    ____   _____   _    ____   _____  ____  ___");
-    System.out.println("             |   |  |        |    /_\\   |        |    |  |  |__");
-    System.out.println("             |   |  |___     |   /   \\  |___     |    |__|  |__");
-    System.out.println("\u001B[37m");
-  }
-}
-
-class testing_1 {
-  public static void main(String[] args) {
-
-    int num = 3;
-    System.out.println("\u001B[3" + num + "m"
-                       + "test");
-  }
-}
