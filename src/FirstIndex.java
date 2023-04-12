5112adc005737bb9d807bbb89c5206c7367da2bf
Meaneac <kyle@schang.net>
2023-04-11 20:17:43 -0400

    working on texts

--- src/FirstIndex.java
+++ src/FirstIndex.java
@@ -12,13 +12,30 @@ public class FirstIndex {
 
 class testing_print {
   public static void main(String[] args) {
+
+    thanks_for_playing();
+    title();
+  }
+
+  public static void thanks_for_playing() {
+    System.out.println();
     System.out.println("\u001B[35m");
     System.out.println(
-        "         ____    _  _____ , ____    ____     _    __    __  ____      ");
+        "_____        _            ___   ___  __   _     _        _                ____");
     System.out.println(
-        "         |      /_\\   |     |___    | ___   /_\\   | \\  / |  |___     ");
+        "  |   |__|  /_\\  |\\ | |/  |__   |__ |  | |_|   |_| |    /_\\   \\_/  | |\\ | | __");
     System.out.println(
-        "         |___  /   \\  |     ___|    |___|  /   \\  |  \\/  |  |___     ");
-    System.out.print("\u001B[37m");
+        "  |   |  | /   \\ | \\| |\\  __|   |   |__| | \\   |   |__ /   \\   |   | | \\| |__|");
+  }
+
+  public static void title() {
+    System.out.print("\u001B[32m");
+    System.out.println(
+        "           _____    ____   _____   _    ____   _____  ____  ___");
+    System.out.println(
+        "             |   |  |        |    /_\\   |        |    |  |  |__");
+    System.out.println(
+        "             |   |  |___     |   /   \\  |___     |    |__|  |__");
+    System.out.println("\u001B[37m");
   }
 }
