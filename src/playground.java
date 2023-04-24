d33b5044df436f98bdfc6117bee8b653cfcfc1d2
Kyle Schang <kyle@schang.net>
2023-04-23 20:13:38 -0400

    lsat quiz

--- src/playground.java
+++ src/playground.java
@@ -1,1 +1,15 @@
 // Playground
+
+class playground {
+  public static void main(String[] args) {
+    double sum = 0;
+    for (double d = 0; d < 10; sum += sum + d) {
+      d += 0.1;
+    }
+    int y = 0;
+    for (int i = 0; i < 10; ++i) {
+      y += 1;
+    }
+    System.out.println(y);
+  }
+}
