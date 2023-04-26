b0d1585233a298572ee8257bbabfa26bc2c8ad78
Kyle Schang <kyle@schang.net>
2023-04-25 20:17:34 -0400

    working in playground

--- src/playground.java
+++ src/playground.java
@@ -1,15 +1,5 @@
 // Playground
 
 class playground {
-  public static void main(String[] args) {
-    double sum = 0;
-    for (double d = 0; d < 10; sum += sum + d) {
-      d += 0.1;
-    }
-    int y = 0;
-    for (int i = 0; i < 10; ++i) {
-      y += 1;
-    }
-    System.out.println(y);
-  }
+  public static void main(String[] args) { System.out.println("Hello World"); }
 }
