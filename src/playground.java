4f448d3973f797336c47fe09b3a8603bdec883ef
Kyle Schang <kyle@schang.net>
2023-04-26 23:01:02 -0400

    studying for exam 2

--- src/playground.java
+++ src/playground.java
@@ -3,3 +3,24 @@
 class playground {
   public static void main(String[] args) { System.out.println("Hello World"); }
 }
+
+interface eddible {
+  public abstract String howToEat();
+}
+
+class Animal {
+  public void message() { System.out.println("This is an animal"); }
+}
+
+class Chicken extends Animal implements eddible {
+  public void message() { System.out.println("This is a chicken"); }
+
+  @Override
+  public String howToEat() {
+    return "Fry it";
+  }
+}
+
+class Tiger extends Animal {
+  public void message() { System.out.println("This is a tiger"); }
+}
