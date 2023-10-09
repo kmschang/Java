32e6a07cb9959a4a9543ec22dee5aeb7ad66bd52
Kyle Schang <kyle@schang.net>
2023-10-09 11:54:22 -0400

    added comments to TicTacToe

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,23 +1,44 @@
+/*
+Kyle Schang
+10-9-23
+Tic Tac Toe Project
+ */
+
+// Importing all the modules needed
 import static java.lang.Character.toUpperCase;
 
 import java.util.*;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
+// Class Tic Tac Toe
 public class TicTacToe {
 
+  // All the variables we need during the program
+  // Size of the board
   public static int dim;
+  // Making the board
   public static char[][] board = new char[dim][dim];
+  // Making the winning board, to display when the game is over
   public static char[][] winning_board = new char[dim][dim];
+  // Declaring what character the play is going to be (X or O)
   public static Character player_char;
+  // Player num corresponds to player_char. If player is X, num = 0, else, num = 1
   public static int player_num;
+  // Declaring what character the computer is
   public static Character computer_char;
+  // Making the scanner so we can read inputs
   static Scanner scnr = new Scanner(System.in);
+  // Starting the move counter on 1
   public static int move = 1;
+  // Used in the winning board
   public static int color_num = 7;
 
+  // Main function used to run the game. Only call's one function "game"
   public static void main(String[] args) { game(); }
 
+  // Function that run the whole game. Starting with loading to get the loading screen, then get the dimensions of the
+  // board, then set characters, then start making moves
   public static void game() {
     loading();
     get_dimensions();
@@ -25,13 +46,16 @@ public class TicTacToe {
     move();
   }
 
+  // Function to clear the screen
   public static void clear(int lines) {
     String blank_line = "\n";
     System.out.print(blank_line.repeat(lines));
   }
 
+  // Loading function
   public static void loading() {
 
+    // Clear screen, display title and creator and start the loading process
     clear(100);
     title();
     System.out.println("\u001B[34m");
@@ -68,6 +92,7 @@ public class TicTacToe {
     title();
   }
 
+  // Title Function to print Title
   public static void title() {
     System.out.print("\u001B[32m");
     System.out.println("           _____    ____   _____   _    ____   _____  ____  ___");
@@ -76,6 +101,8 @@ public class TicTacToe {
     System.out.println("\u001B[37m");
   }
 
+  // Function used at the beginning to display the starting board with color coded numbers to show what each box's
+  // number is
   public static void starting_board() {
     title();
     String line = "----";
@@ -130,12 +157,14 @@ public class TicTacToe {
     }
   }
 
+  // Function used to get the dimensions of the board from the player
   public static void get_dimensions() {
 
+    // Ask question
     System.out.println();
-
     System.out.println("\u001B[34m         What are the dimensions of the Tic Tac Toe board (3-10)?\u001B[37m");
 
+    // Try and get an Int, if not, keep trying
     try {
       dim = Integer.parseInt(scnr.next());
       if (dim < 3 || dim > 10) {
@@ -148,8 +177,10 @@ public class TicTacToe {
     }
   }
 
+  // Function used to get the character the player wishes to use
   public static Character pick_char() {
     System.out.println();
+    // Try to get and input of either X or O, if not, try again
     try {
       System.out.println(
           "\u001B[34m                     Do you want to be \u001B[31m'X'\u001B[34m or \u001B[31m'O'\u001B[37m");
@@ -170,30 +201,38 @@ public class TicTacToe {
     }
   }
 
+  // Function used to set the  characters after getting them in the previous function. Also displays starting board.
   public static void set_chars() {
 
+    // Clear screen and display starting board
     clear(100);
     starting_board();
 
     char player = toUpperCase(pick_char());
     player_char = player;
 
+    // sets the chars for each player
     if (player == 'X') {
       computer_char = 'O';
     } else {
       computer_char = 'X';
     }
 
+    // Initialize the Board array
     board = new char[dim][dim];
 
+    // Display board at the end
     display_board();
   }
 
+  // Function used to display the board
   public static void display_board() {
 
+    // Clears screen and displays the title
     clear(100);
     title();
 
+    // Strings used to make the board
     String line = "---";
     String line2 = "  ";
     String line3;
@@ -209,6 +248,7 @@ public class TicTacToe {
 
     line4 = "--" + line5.repeat(dim - 3);
 
+    // Printing what each player's chars are
     System.out.println();
 
     System.out.println("\u001B[37m                    Player is \u001B[33m" + player_char +
@@ -216,6 +256,7 @@ public class TicTacToe {
 
     System.out.println();
 
+    // Loop used to print the board
     for (int row = 0; row < dim; ++row) {
       for (int col = 0; col < dim; ++col) {
         if (col == 0) {
@@ -241,8 +282,10 @@ public class TicTacToe {
     }
   }
 
+  // Function used to print the winning board once the game is over. Takes in the winning and losing characters
   public static void display_winning_board(Character winning_char, Character losing_char) {
 
+    // Clears display and sets title
     clear(100);
     title();
     System.out.print("\u001B[34m");
@@ -271,6 +314,7 @@ public class TicTacToe {
 
     line4 = "--" + line5.repeat(dim - 3);
 
+    // Check to make sure the winning character won
     winning_check(winning_char);
 
     if (winning_char == player_char) {
@@ -279,6 +323,7 @@ public class TicTacToe {
       color_num = 6;
     }
 
+    // Loop to display the board with colors to easily see how they won
     for (int row = 0; row < dim; ++row) {
       for (int col = 0; col < dim; ++col) {
         if (col == 0) {
@@ -306,15 +351,18 @@ public class TicTacToe {
     }
   }
 
+  // Function used to check if someone has won
   public static void winning_check(Character winning_char) {
 
+    // Initialize the winning board
     winning_board = new char[dim][dim];
 
+    // Fill the array with all blank spaces
     for (int row = 0; row < dim; ++row) {
       Arrays.fill(winning_board[row], ' ');
     }
 
-    // Checking horizontal
+    // Checking horizontal for win
     for (int row = 0; row < dim; ++row) {
       int count = 0;
       for (int col = 0; col < dim; ++col) {
@@ -332,7 +380,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking Vertical
+    // Checking Vertical for win
     for (int row = 0; row < dim; ++row) {
       int count = 0;
       for (int col = 0; col < dim; ++col) {
@@ -350,7 +398,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking upper left diagonal
+    // Checking upper left diagonal for win
     int count = 0;
     for (int row = 0; row < dim; ++row) {
       if (board[row][row] == winning_char) {
@@ -366,7 +414,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking upper right diagonal
+    // Checking upper right diagonal for win
     count = 0;
     for (int row = 0; row < dim; ++row) {
       if (board[row][(dim - 1) - row] == winning_char) {
@@ -383,6 +431,7 @@ public class TicTacToe {
     }
   }
 
+  // Function used to make a move
   public static void move() {
 
     // player gets odd moves, player starts
@@ -390,7 +439,16 @@ public class TicTacToe {
       choose_location();
       display_board();
     }
-    // player gets even moves, computer starts
+
+    /*
+     player gets even moves, computer starts
+     If statements to tell computer what do to and in what order.
+     If it is the first move, they place randomly
+     If it is no the first move, it will check if they can win, if not, they check to block
+     First it checks if they have all but 1 in a direction and if not, it checks if they have all but 2
+     If not any above, it places a random move
+     */
+
     else if (player_num % 2 != move % 2) {
       if (move == 1) {
         computer_random_move();
@@ -412,6 +470,7 @@ public class TicTacToe {
       display_board();
     }
 
+    // At the end of the move, it checks to see if anyone has won. If so, goes to according functions
     if (check_for_win(player_char)) {
       player_won();
     } else if (check_for_win(computer_char)) {
@@ -423,7 +482,9 @@ public class TicTacToe {
     }
   }
 
+  // Function to place a random move for computer
   public static void computer_random_move() {
+    // Get random number
     int row;
     int col;
     int first_index = (int)Math.floor(Math.random() * ((dim * dim) - 1 + 1) + 1);
@@ -433,6 +494,8 @@ public class TicTacToe {
     row = first_index / dim;
     col = first_index % dim;
 
+    // Checks to make sure there is nothing in the location already, if so, it repeats the function, until it find a
+    // suitable spot
     if (check_location(row, col)) {
       board[row][col] = computer_char;
       ++move;
@@ -441,19 +504,23 @@ public class TicTacToe {
     }
   }
 
+  // Function to make sure there is nothing in the spot checked
   public static boolean check_location(int row, int col) {
     return board[row][col] != 'X' && board[row][col] != 'O';
     // This is here so that it won't fit on the same line
   }
 
+  // Function for player to place their next move
   public static void choose_location() {
 
+    // Set variables used
     int location;
     int row;
     int col;
     System.out.println("\n\u001B[34m               Where would like to place the next " + player_char + "? (1-" +
                        (dim * dim) + ")\u001B[37m");
 
+    // Try placing in certain spots and if not, says it can't and re-prompts the user to place another move
     try {
       location = Integer.parseInt(scnr.next());
       if (location < 1 || location > Math.pow(dim, 2)) {
@@ -478,9 +545,10 @@ public class TicTacToe {
     }
   }
 
+  // Check to see if a character has won
   public static boolean check_for_win(char letter) {
 
-    // Checking horizontal
+    // Checking horizontal for win
     for (int row = 0; row < dim; ++row) {
       int count = 0;
       for (int col = 0; col < dim; ++col) {
@@ -493,7 +561,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking Vertical
+    // Checking Vertical for win
     for (int row = 0; row < dim; ++row) {
       int count = 0;
       for (int col = 0; col < dim; ++col) {
@@ -506,7 +574,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking upper left diagonal
+    // Checking upper left diagonal for win
     int count = 0;
     for (int row = 0; row < dim; ++row) {
       if (board[row][row] == letter) {
@@ -517,7 +585,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking upper right diagonal
+    // Checking upper right diagonal for win
     count = 0;
     for (int row = 0; row < dim; ++row) {
       if (board[row][(dim - 1) - row] == letter) {
@@ -528,12 +596,14 @@ public class TicTacToe {
       }
     }
 
+    // If none of the above are true, there is no win and it returns false
     return false;
   }
 
+  // Function to check for the computer to block. Takes in number of how many needed to win to block
   public static boolean computer_block(int num) {
 
-    // Checking upper left diagonal
+    // Checking upper left diagonal for block
     int count = 0;
     int count2 = 0;
     for (int row = 0; row < dim; ++row) {
@@ -554,7 +624,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking upper right diagonal
+    // Checking upper right diagonal for block
     count = 0;
     count2 = 0;
     for (int row = 0; row < dim; ++row) {
@@ -575,7 +645,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking horizontal
+    // Checking horizontal for block
     for (int row = 0; row < dim; ++row) {
       count = 0;
       count2 = 0;
@@ -598,7 +668,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking Vertical
+    // Checking Vertical for block
     for (int row = 0; row < dim; ++row) {
       count = 0;
       count2 = 0;
@@ -620,12 +690,15 @@ public class TicTacToe {
         }
       }
     }
+    // Returns true if it wasn't able to block anything
     return true;
   }
 
+  // Function used to see if a computer can win with num number of moves. Checks 1 first to win the game then checks 2
+  // to win in 2 moves.
   public static boolean computer_win(int num) {
 
-    // Checking horizontal
+    // Checking horizontal for win
     for (int row = 0; row < dim; ++row) {
       int count = 0;
       int count2 = 0;
@@ -648,7 +721,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking Vertical
+    // Checking Vertical for win
     for (int row = 0; row < dim; ++row) {
       int count = 0;
       int count2 = 0;
@@ -671,7 +744,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking upper left diagonal
+    // Checking upper left diagonal for win
     int count = 0;
     int count2 = 0;
     for (int row = 0; row < dim; ++row) {
@@ -692,7 +765,7 @@ public class TicTacToe {
       }
     }
 
-    // Checking upper right diagonal
+    // Checking upper right diagonal for win
     count = 0;
     count2 = 0;
     for (int row = 0; row < dim; ++row) {
@@ -716,6 +789,7 @@ public class TicTacToe {
     return true;
   }
 
+  // Function used to display the player won. Displays the board to show how they won
   public static void player_won() {
 
     if (player_char == 'X') {
@@ -728,9 +802,11 @@ public class TicTacToe {
     System.out.println("                   \\_/  | |  |  |    \\  / \\  /  | |  |\\ |");
     System.out.println("                    |   |_|  |__|     \\/   \\/   |_|  | \\|");
     System.out.print("\u001B[37m");
+    // Asks if they want to play again
     play_again();
   }
 
+  // Function used to display the computer won. Displays the board to show how they won
   public static void computer_won() {
     if (computer_char == 'X') {
       display_winning_board('X', 'O');
@@ -745,6 +821,8 @@ public class TicTacToe {
     play_again();
   }
 
+  // Function used to display it was a tie. Displays the board to show how it was a tie. There are two different ways to
+  // display a tie. Function takes in an int to decide which title it wants to display for a tie
   public static void tie(int num) {
     if (num == 1) {
       display_board();
@@ -765,6 +843,7 @@ public class TicTacToe {
     }
   }
 
+  // Function used to display thanks for playing title at the end
   public static void thanks_for_playing() {
     System.out.println();
     System.out.println("\u001B[35m");
@@ -773,12 +852,16 @@ public class TicTacToe {
     System.out.println("  |   |  | /   \\ | \\| |\\  __|   |   |__| | \\   |   |__ /   \\   |   | | \\| |__|");
   }
 
+  // Function to ask if they wish to play again.
   public static void play_again() {
+
+    // Asks question
     System.out.println();
     System.out.println("\u001B[34m                   Do you want to play again (y or n)?\u001B[37m");
 
     String answer = scnr.next();
 
+    // checks it is y or n, if not, calls the function again
     if (answer.equalsIgnoreCase("y")) {
       clear_board();
       move = 1;
@@ -791,12 +874,14 @@ public class TicTacToe {
     }
   }
 
+  // Function to clear board when restarting
   public static void clear_board() {
     for (int row = 0; row < dim; ++row) {
       Arrays.fill(board[row], ' ');
     }
   }
 
+  // Custom exception to check if they inputted X or O
   static class notCharException extends Exception {
 
     Character choice;
